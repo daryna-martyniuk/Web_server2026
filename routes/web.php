f4526de82f27c79e87b68fb71acbea0d8c69ff78
daryna-martyniuk <daryna.martyniuk7@gmail.com>
2026-05-22 08:51:58 +0000

    feat: (lab3) implement blog rest controllers, routes and soft deletes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\Api\Blog\PostController;
+
+Route::prefix('blog')->group(function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +21,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::apiResource('rest', RestTestController::class)->names('restTest');
