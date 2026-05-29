3b0885b3d375230b416e10e9995fbf10b4dbca1e
daryna-martyniuk <daryna.martyniuk7@gmail.com>
2026-05-29 12:02:51 +0000

    feat: (lab9) implement Eloquent relationships for BlogPost and custom update response data

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers\Blog\Admin;
 
 use App\Http\Requests\BlogCategoryCreateRequest;
-//use App\Http\Controllers\Controller;
 use App\Models\BlogCategory;
 use App\Repositories\BlogPostRepository;
+use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogPostUpdateRequest;
+use Carbon\Carbon;
 use Illuminate\Support\Str;
-//use Illuminate\Http\Request;
 use App\Http\Requests\BlogCategoryUpdateRequest;
+//use App\Http\Controllers\Controller;
+//use Illuminate\Http\Request;
 
 class PostController extends BaseController{
     /**
      * Display a listing of the resource.
      */
-    public function __construct(private BlogPostRepository $blogPostRepository)
-    {
+    public function __construct(
+        private BlogPostRepository $blogPostRepository,
+        private BlogCategoryRepository $blogCategoryRepository // Властивість для категорій
+    ) {
         //parent::__construct();
     }
 
@@ -48,8 +53,32 @@ class PostController extends BaseController{
     /**
      * Update the specified resource in storage.
      */
-    public function update()
+    public function update(BlogPostUpdateRequest $request, $id)
     {
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+        if (empty($item->published_at) && $data['is_published']) { //якщо поле published_at порожнє і нам прийшло 1 в ключі is_published, то
+            $data['published_at'] = Carbon::now(); //генеруємо поточну дату
+        }
+        $result = $item->update($data); //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено',
+                'data' => $item
+            ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
         //dd(__METHOD__);
     }
 
